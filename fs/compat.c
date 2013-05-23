b2bd824e4b4b872d8d998edf2aaf9bd5c04fd332
Al Viro <viro@zeniv.linux.org.uk>
2013-05-22 22:22:04 -0400

    [readdir] constify ->actor
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/compat.c
+++ fs/compat.c
@@ -876,14 +876,13 @@ asmlinkage long compat_sys_old_readdir(unsigned int fd,
 {
 	int error;
 	struct fd f = fdget(fd);
-	struct compat_readdir_callback buf;
+	struct compat_readdir_callback buf = {
+		.ctx.actor = compat_fillonedir,
+		.dirent = dirent
+	};
 
 	if (!f.file)
 		return -EBADF;
-
-	buf.result = 0;
-	buf.dirent = dirent;
-	buf.ctx.actor = compat_fillonedir;
 
 	error = iterate_dir(f.file, &buf.ctx);
 	if (buf.result)
@@ -956,7 +955,11 @@ asmlinkage long compat_sys_getdents(unsigned int fd,
 {
 	struct fd f;
 	struct compat_linux_dirent __user * lastdirent;
-	struct compat_getdents_callback buf;
+	struct compat_getdents_callback buf = {
+		.ctx.actor = compat_filldir,
+		.current_dir = dirent,
+		.count = count
+	};
 	int error;
 
 	if (!access_ok(VERIFY_WRITE, dirent, count))
@@ -965,12 +968,6 @@ asmlinkage long compat_sys_getdents(unsigned int fd,
 	f = fdget(fd);
 	if (!f.file)
 		return -EBADF;
-
-	buf.current_dir = dirent;
-	buf.previous = NULL;
-	buf.count = count;
-	buf.error = 0;
-	buf.ctx.actor = compat_filldir;
 
 	error = iterate_dir(f.file, &buf.ctx);
 	if (error >= 0)
@@ -1043,7 +1040,11 @@ asmlinkage long compat_sys_getdents64(unsigned int fd,
 {
 	struct fd f;
 	struct linux_dirent64 __user * lastdirent;
-	struct compat_getdents_callback64 buf;
+	struct compat_getdents_callback64 buf = {
+		.ctx.actor = compat_filldir64,
+		.current_dir = dirent,
+		.count = count
+	};
 	int error;
 
 	if (!access_ok(VERIFY_WRITE, dirent, count))
@@ -1052,12 +1053,6 @@ asmlinkage long compat_sys_getdents64(unsigned int fd,
 	f = fdget(fd);
 	if (!f.file)
 		return -EBADF;
-
-	buf.current_dir = dirent;
-	buf.previous = NULL;
-	buf.count = count;
-	buf.error = 0;
-	buf.ctx.actor = compat_filldir64;
 
 	error = iterate_dir(f.file, &buf.ctx);
 	if (error >= 0)
